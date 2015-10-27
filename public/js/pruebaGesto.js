0af2a68be6d2165889ab119ab00a2ef6cd761d99
jose <jose.dolz@gmail.com>
2015-10-27 12:30:41 +0100

    Gesture test

---
+++ public/js/pruebaGesto.js
@@ -0,0 +1,22 @@
+/**
+ * Created by jose on 27/10/15.
+ */
+
+var Shake = require('shake.js');
+
+var myShakeEvent = new Shake({
+    threshold: 15 // optional shake strength threshold
+    //timeout: 1000 // optional, determines the frequency of event generation
+});
+
+myShakeEvent.start();
+
+window.addEventListener('shake', shakeEventDidOccur, false);
+
+//function to call when shake occurs
+function shakeEventDidOccur () {
+
+    document.body.style.backgroundColor = "#FF2323";
+
+    alert('shake!');
+}
