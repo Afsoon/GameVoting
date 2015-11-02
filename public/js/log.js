1213d2fe2af2e39333e7ba8deeb167dc9f77635b
Jose <jose.dolz@gmail.com>
2015-11-02 13:02:08 +0100

    Accelerometers logging

--- public/js/log.js
+++ public/js/log.js
@@ -1,7 +1,9 @@
 var socket = io.connect('http://46.101.214.219', { 'forceNew': true });
 
+console.log("Aplicacion iniciada");
+
 socket.on('start', function(){
-	console.log('Conexion establecida');
+	console.log("Conexion establecida");
 })
 
 socket.on('sensors', function(data){
