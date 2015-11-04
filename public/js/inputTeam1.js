f366aa9f9bc7d52791ee2d00e84ef190cfd4a890
Jose <jose.dolz@gmail.com>
2015-11-04 12:42:34 +0100

    Swipe gestures with shake voting

--- public/js/inputTeam1.js
+++ public/js/inputTeam1.js
@@ -1,30 +1,51 @@
+window.onload = function(){
+  
+  var voted = false;
 
-var myShakeEvent = new Shake({
-    threshold: 15, // optional shake strength threshold
-    timeout: 1000 // optional, determines the frequency of event generation
-});
+  //create a new instance of shake.js.
+  var myShakeEvent = new Shake({
+       threshold: 15
+  });
 
-myShakeEvent.start;
+  // start listening to device motion
+  myShakeEvent.start();
 
-window.addEventListener('shake', shakeEventDidOccur, false);
+  // register a shake event
+  window.addEventListener('shake', shakeEventDidOccur, false);
 
-//function to call when shake occurs
-function shakeEventDidOccur () {
+  //shake event callback
+  function shakeEventDidOccur () {
+    if (voted){
+      navigator.vibrate(1000);
+      $("#swipeArea").html("¡HAS VOTADO!<br><br>GRACIAS");
+      $("#swipeArea").css('background-color', '#DD0000');
+    }
+  }
 
-    alert('shake!');
+  var shakeMsg = "<br><br>Agita el movil en alto para votar";
+ 
+  //Enable swiping...
+  $("#swipeArea").swipe( {
+  //Generic swipe handler for all directions
+  swipe:function(event, direction, distance, duration, fingerCount, fingerData) {
+    if(direction == "left"){
+      $(this).html("Saque a la izquierda" + shakeMsg);
+      voted = true;
+    }
+    if(direction == "right"){
+      $(this).html("Saque a la derecha" + shakeMsg);
+      voted = true;
+    }
+            
+  },
+  threshold: 75       
+  });
+  
+
+  function end(){
+    $("#swipeArea").swipe("destroy");
+    window.removeEventListener('shake', shakeEventDidOccur, false);
+    myShakeEvent.stop();
+  }
+
 }
-
-window.removeEventListener('shake', shakeEventDidOccur, false);
-
-myShakeEvent.stop();
-
-$(function() {      
-      //Enable swiping...
-      $("#swipeArea").swipe( {
-        //Generic swipe handler for all directions
-        swipe:function(event, direction, distance, duration, fingerCount, fingerData) {
-          $(this).text("You swiped " + direction );  
-        },
-        threshold: 0       
-      });
-    });
