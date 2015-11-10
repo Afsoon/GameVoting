48bcec0a820b5a24aaf7034b95a967f0f452d6db
Said <said.atrahouch@gmail.com>
2015-11-10 17:04:33 +0100

    Routes

--- spec/empty.spec.js
+++ spec/empty.spec.js
@@ -1,6 +1,8 @@
 /**
  * Created by saidatrahouchecharrouti on 23/10/15.
  */
+var app = require('../app.js');
+
 describe('GET /', function(){
     it('nothing', function(){
         /**asjkdhasjkdhsajd**/
