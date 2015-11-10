e37e1989847aa563cf66ca6eade9c1fd61f4f109
Said <said.atrahouch@gmail.com>
2015-11-10 11:52:07 +0100

    routes team1

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/team1', function(req, res){
+  res.sendfile('/public/inputTeam1.html');
+});
+
 module.exports = router;
