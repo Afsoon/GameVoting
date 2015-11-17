af24ec31e9601ef3f5672054d45f64349e82fdda
Said <said.atrahouch@gmail.com>
2015-11-17 10:06:07 +0100

    Server Dev, events

--- modules/tenisgame.js
+++ modules/tenisgame.js
@@ -2,30 +2,43 @@
  * Created by saidatrahouchecharrouti on 12/11/15.
  */
 var TeamTennis = require('./teamTennis');
-var teamMap;
+
 
 
 function TenisGame(numberTeams, actions){
     if(!(this instanceof TenisGame)){
         return new TenisGame(numberTeams, actions);
     }
-    teamMap = {};
+    var team;
+    this.teamMap = {};
+    
     for(var i = 0; i < numberTeams; i++){
-        teamMap['team' + (i+1)] = new TeamTennis(actions);
+        this.teamMap['team' + (i+1)] = new TeamTennis(actions);
     }
 }
 
-TenisGame.prototype.addVote  = function (team, action) {
-    if(!(team in teamMap)){
+TenisGame.prototype.addVote  = function (team, action, timeout) {
+    if(!(team in this.teamMap)){
         throw new Error('Invalid Team');
     }
-    console.info(teamMap);
-    console.info(teamMap[team]);
-    teamMap[team].addVote(action);  
+    if(timeout){
+        throw new Error('Timeout');
+    }
+    this.teamMap[team].addVote(action);  
 };
 
 TenisGame.prototype.getWinner = function () {
-    return 'Team 1';
+    var team1Action = this.teamMap['team1'].getActionMajority();
+    var team2Action = this.teamMap['team2'].getActionMajority();
+    
+    if (team1Action === 'none' && team2Action === 'none'){
+        return 'A draw';
+    }else if(team2Action === team1Action || (team1Action === 'none' && team2Action !== 'none')){
+        return 'Team 2';
+    }else{
+        return 'Team 1';
+    }
+    
 };
 
 module.exports = TenisGame;
--- server/serverDev.js
+++ server/serverDev.js
@@ -7,9 +7,98 @@ var path = require('path');
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 var routes = require('../routes/index');
+var Game = require('./../modules/tenisgame');
+var Players = require('./../modules/players');
+var playersInstance = new Players(2, ['service', 'reception']);
+var gameInstance = new Game(2, {'left': 0, 'right': 0});
 
 app.use('/', routes);
+
+var COUNTDOWNDEFAULT = 90;
+var countdown = -1;
+var language;
+var timeout = false;
+var started = false;
+var timestamp;
+
+setInterval(function(){
+    if(countdown>0 ){
+        countdown--;
+        io.sockets.emit('time', countdown);
+        if(countdown == 0 || endVoting()){
+            timeout = true;
+            started = false;
+            updateVotes();
+            io.sockets.emit('finishedTime');
+        }
+    }
+}, 1000);
 
 server.listen(9000, function(){
     console.log("Servidor iniciado");        
 });
+
+
+io.on('connection', function(socket){
+
+    socket.emit('side', playersInstance.addPlayer());
+    generateSessionID();
+    if(started){
+        generateStatusJSON(socket);
+    }
+    
+    socket.on('start', function (data) {
+        started = true;
+        var informationScoreboard = JSON.parse(data);
+        setCountdown(informationScoreboard['countdownSeconds']);
+        setLanguage(informationScoreboard['language']);
+        socket.emit('startScoreboard', informationScoreboard['language']);
+        var informationPlayer = {};
+        informationPlayer['sessionID'] = generateSessionID();
+        socket.emit('startPlayer', informationPlayer);
+    });
+
+    socket.on('team1', function (data){
+        gameInstance.addVote('team1', data, timeout);
+    });
+
+
+    socket.on('team2', function (data) {
+        gameInstance.addVote('team2', data, timeout);
+    });
+    
+    socket.on('getStatus', function () {
+        generateStatusJSON(socket);
+    })
+});
+
+function setCountdown(seconds){
+    if(seconds === 'default'){
+        countdown = COUNTDOWNDEFAULT;
+    }else{
+        countdown = seconds;
+    }
+}
+
+function generateStatusJSON(socket){
+    var informationServer = {};
+    informationServer['canVote'] = started;
+    informationServer['sessionID'] = timestamp;
+    socket.emit('status', informationServer);
+}
+
+function setLanguage(languageGame){
+    language = languageGame;
+}
+
+function generateSessionID(){
+    if(started){
+        timestamp = Date.now();
+        return timestamp;
+    }
+    
+    if(timestamp === undefined){
+        timestamp = Date.now();
+        return timestamp;
+    }
+}
