e32cfc551691538eb0381f223786d62d3ddabf98
Jose <jose.dolz@gmail.com>
2015-11-17 13:44:27 +0100

    Finished Control Panel Fixed scoreboard and input view

--- public/js/controlPanel.js
+++ public/js/controlPanel.js
@@ -1,18 +1,20 @@
 var socket = io.connect('http://46.101.214.219:9000', { 'forceNew': true });
 var language;
 
-function loadConfig(lang){
+function loadConfig(lang) {
     language = lang;
 }
 
-function start(){
+function setup() {
+	socket.emit('setupInstructions' , language);
+	$("#startButton").prop("disabled", false);
+}
+
+function start() {
 	var seconds = $("#countdown").val();
 	if (seconds==="") {seconds="default";}
-
 	language = $("select").val();
-	
 	var json = {'language': language, 'countdownSeconds': seconds};
-	
-	console.log(json);
 	socket.emit('start', json);
+	$("#startButton").prop("disabled",true);
 }
