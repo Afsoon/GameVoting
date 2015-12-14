f091ae699ef08a162b64c2fcfa0f15e289f868f4
Said <said.atrahouch@gmail.com>
2015-12-14 10:55:02 +0100

    client animations and event handler

--- public/js/index-new.js
+++ public/js/index-new.js
@@ -3,6 +3,7 @@
  */
 $(function() {
     var teamSide, sideVote, token, GAMEVOTING;
+    var comingFromGame = false;
     var language = navigator.language || navigator.userLanguage;
     var supportsVibrate = "vibrate" in navigator;
     var voted = false;
@@ -49,7 +50,7 @@ $(function() {
             GAMEVOTING = data;
             socket.on('side', function(data){
                 teamSide = data;
-                $("#text").append("<h1>"+GAMEVOTING[teamSide + "Name"]+"</h1>");
+                $("#text").append("<h2>"+GAMEVOTING[teamSide + "Name"]+"</h2>");
             });
             socket.on('status', function(status){
                 checkStatus(status);
@@ -114,29 +115,27 @@ $(function() {
     }
 
     function initApp() {
+        var elementAnimated = document.getElementById("textGame");
+        elementAnimated.addEventListener("animationend", animationToGameListener, false);
         $("#textGame").toggleClass('none');
         $("#textIns").toggleClass('none');
-        animationToGame();
         
     }
     
-    function animationToGame(){
-        setTimeout(function () {
-            animationShowInstructionGame();
-            $("#article").addClass('gone');
-            $("#juego").addClass('slide-2');
-            
-            myShakeEvent.start();
-            addEventListener('shake', shakeEventCallback, false);
-            enableSwipe();
-        }, 1200)
+    function animationToGameListener(e){
+        switch(e.type){
+            case "animationend":
+                animationShowInstructionGame();
+                $("#juego").addClass('slide-2');
+                myShakeEvent.start();
+                addEventListener('shake', shakeEventCallback, false);
+                enableSwipe();
+                break;
+        }
     }
     
     function animationShowInstructionGame(){
-        setTimeout(function () {
-            $("#info").append("<h2>"+GAMEVOTING[teamSide + "InitMsg"]+"</h2>").addClass('up');
-            $("#arrow").append("<h3> > </h3>").addClass('down');
-        }, 1000);
+        $("#swipeArea").text(GAMEVOTING[teamSide + "InitMsg"]);
     }
 
     function showWait() {
@@ -155,9 +154,14 @@ $(function() {
         if ((direction === "left") || (direction === "right")) {
             var msgType = getMessageType();
             var msg = msgType + direction.replace(/^./, direction[0].toUpperCase());
-            area.html(GAMEVOTING[msg] + "<br><br>" + GAMEVOTING.shakeMsg);
+            if($("#option").length > 0){
+                $("#option").removeClass('showArrow');    
+            }
+            area.html("<p id='option' >"+GAMEVOTING[msg] + "<br><br>" + GAMEVOTING.shakeMsg + "</p>");
+            $("#option").addClass('showArrow');
             voted = true;
             sideVote = direction;
+            comingFromGame = true;
         }
     }
 
@@ -178,12 +182,37 @@ $(function() {
     }
 
     function showVoted() {
+        socket.on('winner', function(side){
+            if(teamSide === side){
+               console.log('winner');
+            }else{
+                console.log('loser');
+            }
+            socket.disconnect();
+        });
+
+        if(!comingFromGame){
+            showVotedAnimation();
+        }else{
+            addMessageVoted();
+        }
+    }
+
+    function addMessageVoted() {
+        var fastTrasitionAnimation = document.getElementById("juego");
         var tweet = convertString(encodeURI(GAMEVOTING.tweet));
         if(supportsVibrate) { navigator.vibrate(1000); }
-        $("#swipeArea").html(GAMEVOTING.votedMsg + "<br><br>" + GAMEVOTING.thanksMsg + "<br><br>");
-        $("#swipeArea").css('background-color', '#AD0505');
-        $("#swipeArea").append("<a href='https://twitter.com/intent/tweet?text="+ tweet + "'>"
-            +"<img src='images/tweetbutton.png' alt='Tweet this!' id='tweet'></a>")
+        $("#swipeArea").html("");
+        $("#swipeArea").append("<p id='voted' >"+GAMEVOTING.votedMsg + "<br><br>" + GAMEVOTING.thanksMsg + "<br><br>" + "</p> <a href='https://twitter.com/intent/tweet?text="+ tweet + "'>"
+            +"<img src='images/tweetbutton.png' alt='Tweet this!' id='tweet'></a>").addClass('showVoted');
+        $("#voted").addClass('showArrow');
+        fastTrasitionAnimation.removeEventListener("animationend", addMessageVoted, false);
+    }
+
+    function showVotedAnimation(){
+        var fastTrasitionAnimation = document.getElementById("juego");
+        fastTrasitionAnimation.addEventListener("animationend", addMessageVoted, false);
+        $("#juego").addClass('goToVotedSecondSlide');
     }
 
     function convertString(s) {
@@ -206,6 +235,5 @@ $(function() {
         window.removeEventListener('shake', shakeEventCallback, false);
         myShakeEvent.stop();
         $("#swipeArea").swipe("destroy");
-        socket.disconnect();
     }
 });
